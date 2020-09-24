582319cf33b6fec2d8c6e3bc5fd21ed869e9ba8e
jinghan <jinghan99@qq.com>
2020-09-24 22:45:10 +0800

    完成定时任务

--- girl-reptile/src/main/docker/docker-app-start.sh
+++ girl-reptile/src/main/docker/docker-app-start.sh
@@ -3,7 +3,7 @@
 AppName=app.jar
 
 #JVM参数
-JVM_OPTS="-Dname=$AppName  -Duser.timezone=Asia/Shanghai -Xms256M -Xmx256M -XX:PermSize=64M -XX:MaxPermSize=125M -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
+JVM_OPTS="-Dname=$AppName  -Duser.timezone=Asia/Shanghai -Xms512M -Xmx512M -XX:PermSize=125M -XX:MaxPermSize=256M -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
 APP_HOME=`pwd`
 LOG_PATH=$APP_HOME/$AppName.log
 
